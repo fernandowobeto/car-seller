29064dd76097c4fd25663342c6d9178ab3da6e82
Fernando Wobeto <fernandowobeto@gmail.com>
2019-05-09 19:57:02 -0300

    criacao tabela de cnfiguracoes

---
+++ app/Entities/Configuracao.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Configuracao extends Model
+{
+
+   protected $table = 'configuracoes';
+
+}
---
+++ database/migrations/2019_05_08_014745_create_configuracoes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConfiguracoesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('configuracoes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('bem_vindo');
+            $table->text('sobre');
+            $table->text('servicos');
+            $table->text('depoimentos');
+            $table->text('noticias');
+            $table->text('quem_somos');
+            $table->text('nossa_missao');
+            $table->text('porque_escolher');
+            $table->integer('dias_anuncios')->default(30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('configuracoes');
+    }
+}
